c16d5696f6beca85bb30c8ef8547fb34c6ab8e3b
Brianna LaCasse <briannalacasse@Briannas-MacBook-Pro.local>
2020-10-09 21:52:23 -0400

    library hooked up

---
+++ client/src/components/library/Library.jsx
@@ -0,0 +1,130 @@
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
+import Layout from '../shared/Layout';
+import styled from 'styled-components';
+import BookCard from '../library/BookCard'
+
+const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 20px auto;
+  text-align: center;
+`
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 60px auto;
+`
+
+const Header = styled.h3`
+  font-size: 48px;
+`
+
+const SubHeader = styled.h4`
+  font-size: 24px;
+  font-weight: 400;
+  margin: 0;
+`
+
+const SectionPanel = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 30px;
+  background-color: #DFE1E1;
+  border: 2px solid #9AB1A9;
+  padding-bottom: 70px;
+  box-shadow: 1px 1px 1px 1px #9AB1A9;
+  border-radius: 5px;
+`
+
+const SectionHeading = styled.h4`
+  font-size: 36px;
+  color: #4A4C4B;
+  margin: 50px;
+  font-weight: 300;
+`
+
+const IconPanel = styled.div`
+  display: flex;
+  margin-left: 20px;
+`
+
+const Icons = styled.i`
+  font-size: 6rem;
+  margin-left: 50px;
+  margin-right: 50px;
+`
+
+const Library = () => {
+  const [books, setBooks] = useState([]);
+  const [business, setBusiness] = useState([]);
+  const [industry, setIndustry] = useState([]);
+  const [fetchEntries, setFetchEntries] = useState(false);
+
+  useEffect(() => {
+    const getBooks = async () => {
+      const airtableURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/books`;
+      const response = await axios.get(airtableURL, {
+        headers: {
+          'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
+        },
+      });
+      setBooks(response.data.records); 
+    }
+    getBooks();
+  }, [fetchEntries]);
+
+  useEffect(() => {
+    const industryBooks = books.filter((element) => element.fields.category === "industry")
+    setIndustry(industryBooks);
+    
+    const businessBooks = books.filter((element) => element.fields.category === "business")
+    setBusiness(businessBooks);
+  }, [books]);
+
+    
+  const industryBooksJSX = industry.map((book, index) => (
+    <BookCard
+      key={index}
+      name={book.fields.name}
+      author={book.fields.author}
+      id={book.id}
+    />
+  ));
+
+  const businessBooksJSX = business.map((book, index) => (
+    <BookCard
+      key={index}
+      name={book.fields.name}
+      author={book.fields.author}
+      id={book.id}
+    />
+  ));
+
+
+  return (
+    <Layout>
+      <HeaderContainer>
+        <Header>Library</Header>
+        <SubHeader>Here are some of the titles I found most influential!</SubHeader>
+      </HeaderContainer>
+      <MainContainer>
+        <SectionPanel>
+          <SectionHeading>Industry</SectionHeading>
+          <IconPanel>
+            {industryBooksJSX}
+          </IconPanel>
+        </SectionPanel>
+        <SectionPanel>
+          <SectionHeading>Business</SectionHeading>
+          <IconPanel>
+            {businessBooksJSX}
+          </IconPanel>
+        </SectionPanel>
+      </MainContainer>
+    </Layout>
+  )
+}
+
+export default Library;
